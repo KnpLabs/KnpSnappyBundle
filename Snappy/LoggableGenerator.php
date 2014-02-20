f921c4b9ae3ab69d06b4053b1a8a484786bdb593
Sean Gofus <sean.gofus@advisorsexcel.com>
2014-02-20 11:08:52 -0600

    Changed sprintf to vsprintf to handle arrays that are passed to it. This is to support the commit from the pull request: https://github.com/KnpLabs/snappy/pull/75 that handles multiple inputs.

--- Snappy/LoggableGenerator.php
+++ Snappy/LoggableGenerator.php
@@ -41,7 +41,7 @@ class LoggableGenerator implements GeneratorInterface
      */
     public function generate($input, $output, array $options = array(), $overwrite = false)
     {
-        $this->logDebug(sprintf('Generate from file (%s) to file (%s).', $input, $output));
+        $this->logDebug(vsprintf('Generate from file (%s) to file (%s).', $input, $output));
 
         $this->generator->generate($input, $output, $options, $overwrite);
     }
@@ -61,7 +61,7 @@ class LoggableGenerator implements GeneratorInterface
      */
     public function getOutput($input, array $options = array())
     {
-        $this->logDebug(sprintf('Output from file (%s).', $input));
+        $this->logDebug(vsprintf('Output from file (%s).', $input));
 
         return $this->generator->getOutput($input, $options);
     }
