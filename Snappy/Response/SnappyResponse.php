a84c6e53301f96d985ee8cf956ca2311dcc8cd46
Andrew Kovalyov <andrew.kovalyoff@gmail.com>
2015-05-05 15:14:34 +0200

    Use ResponseHeadersBag constants instead of strings for Content-Disposition validation

--- Snappy/Response/SnappyResponse.php
+++ Snappy/Response/SnappyResponse.php
@@ -3,12 +3,13 @@
 namespace Knp\Bundle\SnappyBundle\Snappy\Response;
 
 use Symfony\Component\HttpFoundation\Response as Base;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 abstract class SnappyResponse extends Base
 {
     public function __construct($content, $fileName, $contentType, $contentDisposition = 'attachment', $status = 200, $headers = array())
     {
-        $contentDispositionDirectives = array('inline', 'attachment');
+        $contentDispositionDirectives = array(ResponseHeaderBag::DISPOSITION_INLINE, ResponseHeaderBag::DISPOSITION_ATTACHMENT);
         if (!in_array($contentDisposition, $contentDispositionDirectives)) {
             throw new \InvalidArgumentException(sprintf('Expected one of the following directives: "%s", "%s" given.', implode('", "', $contentDispositionDirectives), $contentDisposition));
         }
