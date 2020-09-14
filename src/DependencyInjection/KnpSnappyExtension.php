20af46ba9a67c2c11b99ba1c998629606bac20cf
Massimiliano Arione <garakkio@gmail.com>
2020-09-14 13:48:41 +0200

    move config to root directory

--- src/DependencyInjection/KnpSnappyExtension.php
+++ src/DependencyInjection/KnpSnappyExtension.php
@@ -15,7 +15,7 @@ class KnpSnappyExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
 
         $configuration = new Configuration();
         $processor = new Processor();
